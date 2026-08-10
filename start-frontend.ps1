886e706ecd42930189c64819c14ace8f62210d90
W.T Mahagmage <winukauni@gmail.com>
2026-08-10 15:46:37 +0530

    fix frontend backend connecting dependencies

---
+++ start-frontend.ps1
@@ -0,0 +1,2 @@
+Set-Location "C:\Users\winub\OneDrive\Documents\GitHub\talentpipe-frontend"
+npm run dev 2>&1 | Out-File -FilePath "C:\Users\winub\OneDrive\Documents\GitHub\talentpipe-frontend\frontend.log" -Encoding utf8
